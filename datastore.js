29af4439912035fda1c8aba1672f211ae978676f
Seth Clydesdale <sethc1995@gmail.com>
2015-07-10 10:48:17 -0400

    Create datastore.js

---
+++ datastore.js
@@ -0,0 +1,45 @@
+// Methods to make expirable web storage super short and easy
+// Developed by Seth Clydesdale (https://github.com/SethClydesdale/dataStore-JS)
+(function() {
+  if (typeof dataStore === 'undefined') {
+    
+    window.dataStore = {
+      on : window.localStorage ? true : false,
+      
+      set : function(name, value, exp) {
+        if (dataStore.on) {
+          window.localStorage[name] = value;
+          if (exp) window.localStorage[name + 'Exp'] = +new Date + '|' + exp;
+        }
+      },
+    
+      get : function(name) {
+        var o = {
+          data : null,
+          exp : null
+        };
+        
+        if (dataStore.on) {
+          var exp = window.localStorage[name + 'Exp'];
+          if (exp) {
+            exp = exp.split('|');
+            exp = exp.length == 2 ? +exp[0] - (+new Date - +exp[1]) : 0;
+          }
+          
+          o.data = window.localStorage[name] || '';
+          o.exp = exp > 0 ? exp : 0;
+        }
+        
+        return o;
+      },
+      
+      del : function(name) {
+        if (dataStore.on && window.localStorage[name]) {
+          window.localStorage.removeItem(name);
+          window.localStorage.removeItem(name + 'Exp');
+        }
+      }
+    };
+    
+  } else console && console.warn('dataStore is already initialized');
+})();
